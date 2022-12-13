c0f9c0dce2162a22765036bd81b30d6eed7ec828
iury fukuda <iuryfukuda@gmail.com>
2022-12-12 22:43:52 -0300

    add dotnix run support

--- config.el
+++ config.el
@@ -280,8 +280,49 @@
         :desc "cargo run (custom)" "r" #'my-cargo-run
         :desc "cargo run" "R" #'rustic-cargo-run)
 
+;; Run command in async mode and display buffer
+(defun execute-commands (buffer &rest commands)
+  "Execute a list of shell commands sequentially"
+  (with-current-buffer buffer
+    (set (make-local-variable 'commands-list) commands)
+    (start-next-command)))
 
+(defun start-next-command ()
+  "Run the first command in the list"
+  (if (null commands-list)
+      (insert "\nDone.")
+    (let ((command  (car commands-list)))
+      (setq commands-list (cdr commands-list))
+      (insert (format ">>> %s\n" command))
+      (let ((process (start-process-shell-command command (current-buffer) command)))
+        (set-process-sentinel process 'sentinel)))))
 
+(defun sentinel (p e)
+  "After a process exited, call `start-next-command' again"
+  (let ((buffer (process-buffer p)))
+    (when (not (null buffer))
+      (with-current-buffer buffer
+        ;(insert (format "Command `%s' %s" p e) )
+        (start-next-command)))))
+
+;; Example use
+(defun dotnix-run ()
+  (interactive)
+  (with-current-buffer (get-buffer-create "*dotnix*") (erase-buffer))
+  (create-file-buffer "*dotnix*")
+  (with-current-buffer (get-buffer-create "*dotnix*") (read-only-mode))
+  (execute-commands "*dotnix*"
+                    "dotnix && echo"))
+
+(defun dotnix-run()
+  (interactive)
+  (setq bufname "*dotnix*")
+  (async-shell-command "dotnix && echo Done!" bufname)
+  (with-current-buffer (get-buffer-create bufname) (read-only-mode))
+  (with-current-buffer (get-buffer-create bufname) (evil-normal-state)))
+
+(map! :leader
+  :desc "run" "d" #'dotnix-run)
 
 (setq smudge-transport 'connect)
 (setq display-line-numbers-type nil)
